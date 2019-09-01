4b4046a0a7e596808986013caff491f29200c148
Hououin <hzoraiz8@gmail.com>
2019-09-01 16:12:05 +0500

    recommit

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 function operate(n1, n2, op) {
     switch(op) {
         case "/":
-            visual = String(n1 / n2);
+            visuals = String(n1 / n2);
             display.textContent = (n1 / n2);
             break;
         case "*":
-            visual = String(n1 * n2);
+            visuals = String(n1 * n2);
             display.textContent = (n1 * n2);
             break;
         case "-":
-            visual = String(n1 - n2);
+            visuals = String(n1 - n2);
             display.textContent = (n1 - n2);
             break;
         case "+":
-            visual = String(n1 + n2);
+            visuals = String(n1 + n2);
             display.textContent = (n1 + n2);
             break;
     }
 }
 
-function converter(visual) {
+function converter(visuals) {
     let n1 = "";
     let n2 = "";
     let op = "";
 
-    for(let i = 0; i < visual.length; i++) {
-        if(visual[i] == "/" || visual[i] == "*" || visual[i] == "-" || visual[i] == "+") {
-            op = visual[i];
+    for(let i = 0; i < visuals.length; i++) {
+        if(visuals[i] == "/" || visuals[i] == "*" || visuals[i] == "-" || visuals[i] == "+") {
+            op = visuals[i];
             i++;
-            for(; i < visual.length; i++) {
-                n2 += visual[i];
+            for(; i < visuals.length; i++) {
+                n2 += visuals[i];
             }
             break;
         }
-        n1 += visual[i];
+        n1 += visuals[i];
     }
     operate(Number(n1), Number(n2), op);
 ;}
 
-let visual = "";
+let visuals = "";
 let operatorDouble = false;
 
 const display = document.querySelector('.screen p');
@@ -49,28 +49,28 @@ numbers.forEach(function(number) {
     number.addEventListener("click", function(e) {
         if(e.target.textContent == "/" || e.target.textContent == "*" || e.target.textContent == "-" || e.target.textContent == "+") {
             operatorDouble = true;
-            converter(visual);
+            converter(visuals);
         }
-        visual += e.target.textContent;
-        display.textContent = visual;
+        visuals += e.target.textContent;
+        display.textContent = visuals;
     });
 });
 
 const equals = document.querySelector(".equals");
 equals.addEventListener("click", function(){
-    converter(visual);
+    converter(visuals);
 });
 
 const remove = document.querySelector(".remove");
 remove.addEventListener("click", function() {
-    visual = visual.split("");
-    visual.splice((visual.length - 1),1);
-    visual = visual.join("");
-    display.textContent = visual;
+    visuals = visuals.split("");
+    visuals.splice((visuals.length - 1),1);
+    visuals = visuals.join("");
+    display.textContent = visuals;
 });
 
 const clear = document.querySelector(".clear");
 clear.addEventListener("click", function() {
-    visual = "";
-    display.textContent = visual;
+    visuals = "";
+    display.textContent = visuals;
 });
